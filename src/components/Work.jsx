5381daebd2486f7c27fef2ea862626477877d000
MJR <jraza3363@gmail.com>
2025-04-28 10:52:05 +0500

    work update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -19,34 +19,40 @@ const Work = () => {
           {project.map((item, index) => (
             <div
               key={index}
-              style={{ backgroundImage: `url(${item.image})` }}
               className="shadow-lg shadow-[#040c16] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div "
+              flex justify-center text-center items-center mx-auto content-div overflow-hidden"
             >
-              {/* Hover effect for images */}
-              <div className="opacity-0 group-hover:opacity-100 ">
-                <span className="text-2xl font bold text-white tracking-wider ">
-                  {item.name}
-                </span>
-                <div className="pt-8 text-center ">
-                  {/* eslint-disable-next-line */}
-                  <a href={item.github} target="_blank">
-                    {/* <button
-            className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
-          >
-            Code
-          </button> */}
-                  </a>
-                  {/* eslint-disable-next-line */}
-                  <a href={item.live} target="_blank">
-                    {/* <button
-            className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
-          >
-            Live
-          </button> */}
-                  </a>
+              <div 
+                style={{ backgroundImage: `url(${item.image})` }}
+                className="w-full h-full flex justify-center items-center content-div"
+              >
+                {/* Hover effect for images */}
+                <div className="opacity-0 group-hover:opacity-100 w-full h-full flex flex-col justify-center items-center">
+                  <span className="text-2xl font-bold text-white tracking-wider">
+                    {item.name}
+                  </span>
+                  <div className="pt-8 text-center">
+                    {/* eslint-disable-next-line */}
+                    <a href={item.github} target="_blank">
+                      {item.github && (
+                        <button
+                          className="text-center rounded-lg px-4 py-3 m-2
+                        bg-white text-gray-700 font-bold text-lg"
+                        >
+                          Code
+                        </button>
+                      )}
+                    </a>
+                    {/* eslint-disable-next-line */}
+                    <a href={item.live} target="_blank">
+                      <button
+                        className="text-center rounded-lg px-4 py-3 m-2
+                        bg-white text-gray-700 font-bold text-lg"
+                      >
+                        Live
+                      </button>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
--- src/data/data.js
+++ src/data/data.js
@@ -1,20 +1,53 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
+// import WorkIm from '../assets/projects/workImg.jpeg'
+// import RealEst from '../assets/projects/realestate.jpg'
+import OrmerImg from '../assets/projects/ormer.png'
+import PitchedinImg from '../assets/projects/pitched.in.png'
+import BraggingRightsImg from '../assets/projects/bragging_rights.png'
+import IntroCardImg from '../assets/projects/intro_card.png'
+import JavedImg from '../assets/projects/javed.io.png'
+import XDStudioImg from '../assets/projects/xdstudio.png'
 
 export const data=[
     {
         id:1,
-        name:"React JS Application",
-        image:WorkIm
+        name:"Ormer Frontend",
+        image:OrmerImg,
+        github: "",
+        live: "https://ormer-frontend-nur83.ondigitalocean.app/desktop/discover"
     },
     {
         id:2,
-        name:"React JS Application",
-        image:RealEst
+        name:"PitchDin",
+        image:PitchedinImg,
+        github: "",
+        live: "https://pitchdin.com/login"
     },
     {
         id:3,
-        name:"React JS Application",
-        image:WorkIm
+        name:"Ultimate Bragging Rights",
+        image:BraggingRightsImg,
+        github: "",
+        live: "https://ultimatebraggingrights.com/"
+    },
+    {
+        id:4,
+        name:"XD Studios",
+        image:XDStudioImg,
+        github: "",
+        live: "http://xdstudios.io/"
+    },
+    {
+        id:5,
+        name:"Javed Portfolio",
+        image:JavedImg,
+        github: "",
+        live: "https://javed.io/"
+    },
+    {
+        id:6,
+        name:"Introcard App",
+        image:IntroCardImg,
+        github: "",
+        live: "https://introcard.app/"
     }
 ]
